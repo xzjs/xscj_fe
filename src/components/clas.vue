2066af46dfdc70413e96de8d717e15b87b25eb35
xzjs <2310976219@qq.com>
2018-03-31 15:29:04 +0800

    填充teacher组件

--- src/components/clas.vue
+++ src/components/clas.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>hello</div>
+  <div class="add_btn_box">
+    <el-button type="primary" size="small" icon="el-icon-plus" @click="addTeacherDialog = true"></el-button>
+  </div>
+  <el-table :data="teachersTable">
+    <el-table-column
+      type="index"
+      width="60">
+    </el-table-column>
+    <el-table-column prop="name" label="姓名" >
+    </el-table-column>
+    <el-table-column label="操作">
+      <template slot-scope="scope">
+        <el-button
+          size="mini"
+          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+        <el-button
+          size="mini"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <script>
